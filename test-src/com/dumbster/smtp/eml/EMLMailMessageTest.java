6d90731880a3dcdcf3f416f8a07913b76d9ff84b
RJ Osborne <rjo1970@gmail.com>
2013-07-17 17:44:35 -0400

    added some more test coverage to EMLMailMessage

--- test-src/com/dumbster/smtp/eml/EMLMailMessageTest.java
+++ test-src/com/dumbster/smtp/eml/EMLMailMessageTest.java
@@ -1,10 +1,15 @@
 package com.dumbster.smtp.eml;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class EMLMailMessageTest {
 
@@ -12,7 +17,7 @@ public class EMLMailMessageTest {
     /*
         Example From http://en.wikipedia.org/wiki/Simple_Mail_Transfer_Protocol
     */
-    private String text =
+    private final String text =
             "From: \"Bob Example\" <bob@example.org>\n" +
                     "To: \"Alice Example\" <alice@example.com>\n" +
                     "Cc: theboss@example.com\n" +
@@ -23,21 +28,45 @@ public class EMLMailMessageTest {
                     "This is a test message with 5 header fields and 4 lines in the message body.\n" +
                     "Your friend,\n" +
                     "Bob\n";
+
+    @Before
+    public void setup() {
+        message = new EMLMailMessage(new ByteArrayInputStream(text.getBytes()));
+    }
+
     @Test
     public void testReadHeaders() {
-        message = new EMLMailMessage(new ByteArrayInputStream(text.getBytes()));
         String[] from = message.getHeaderValues("From");
         assertEquals(1, from.length);
         assertEquals("\"Bob Example\" <bob@example.org>", from[0]);
         assertEquals(1, message.getHeaderValues("To").length);
+
+        String[] missing = message.getHeaderValues("MissingHeader");
+        assertNull(missing);
     }
 
     @Test
     public void testGetBody() {
-        message = new EMLMailMessage(new ByteArrayInputStream(text.getBytes()));
         assertEquals("Hello Alice.\n" +
                 "This is a test message with 5 header fields and 4 lines in the message body.\n" +
                 "Your friend,\n" +
                 "Bob", message.getBody());
+    }
+
+    @Test
+    public void testGetHeaderNames() {
+        List<String> headers = new ArrayList<String>();
+        Iterator iterator = message.getHeaderNames();
+        while(iterator.hasNext()) {
+            headers.add((String) iterator.next());
+        }
+        assertEquals(5, headers.size());
+    }
+
+    @Test
+    public void testGetFirstHeaderValue() {
+        String firstFrom = message.getFirstHeaderValue("From");
+        assertEquals("\"Bob Example\" <bob@example.org>", firstFrom);
+        assertNull(message.getFirstHeaderValue("MissingHeader"));
     }
 }
