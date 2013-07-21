1a2c11f96661a208dcb0d9ffbbba67a97f33ceb4
RJ Osborne <rjo1970@gmail.com>
2013-07-21 10:03:00 -0400

    reworked the help message and created an exit condition on an infinite loop

--- src/com/dumbster/smtp/Main.java
+++ src/com/dumbster/smtp/Main.java
@@ -7,36 +7,35 @@ public class Main {
         if (shouldShowHelp(args) || serverOptions.valid == false) {
             showHelp();
             System.exit(1);
-        } else {
-            SmtpServerFactory.startServer(serverOptions);
         }
+
+        SmtpServerFactory.startServer(serverOptions);
     }
 
     private static boolean shouldShowHelp(String[] args) {
         if (args.length == 0)
             return true;
         for (String arg : args) {
-            if ("--help".equals(arg) || "-h".equals(arg)
-                    || "/?".equals(arg))
+            if ("--help".equalsIgnoreCase(arg) || "-h".equalsIgnoreCase(arg))
                 return true;
         }
         return false;
     }
 
     private static void showHelp() {
+        System.out.println();
+        System.out.println("Dumbster Fake SMTP Server");
         System.out.println("usage: java -jar dumbster.jar [options] [port]");
         System.out.println("Starts the SMTP server in the given port. Default port is 25.");
-        System.out.println("");
+        System.out.println();
         System.out.println("Options:");
-        System.out.println("\t -h, --help \t\t this message");
-        System.out.println("\t --mailStore=MAIL_STORE \t define the MailStore used as back end of SMTP server");
-        System.out.println("\t\t\t\t Possible values: EMLMailStore, RollingMailStore");
-        System.out.println("\t --threaded[=THREADED] \t toggles if the SMTP server is single or multi-threaded. THREADED is 'true' or 'false'. Default is true.");
-        System.out.println("");
+        System.out.println("\t-h, --help this message");
+        System.out.println("\t--mailStore=EMLMailMessage Use a file-based mail store");
         System.out.println("MailStores:");
-        System.out.println("\t EMLMailStore \t\t Save messages in EML files");
-        System.out.println("\t RollingMailStore \t Store messages in memory. Only the last 100 messages will be kept in memory. This is the default.");
-        System.out.println("");
+        System.out.println("\tRollingMailStore (Default)  Store messages in memory. Only the last 100 messages will be kept in memory");
+        System.out.println("\tEMLMailStore Save messages in EML files");
+        System.out.println();
+        System.out.println("\t--threaded=false Forces the SMTP server to be single-threaded.");
     }
 
 }
--- src/com/dumbster/smtp/SmtpServerFactory.java
+++ src/com/dumbster/smtp/SmtpServerFactory.java
@@ -19,7 +19,6 @@ public class SmtpServerFactory {
         return server;
     }
 
-
     private static SmtpServer wireUpServer(ServerOptions options) {
         SmtpServer server = new SmtpServer();
         server.setPort(options.port);
@@ -40,6 +39,16 @@ public class SmtpServerFactory {
 
     private static void startServerThread(SmtpServer server) {
         new Thread(server).start();
-        while(! server.isReady()) {}
+        int timeout=1000;
+        while(! server.isReady()) {
+            try {
+                Thread.sleep(1);
+                timeout--;
+                if (timeout < 1) {
+                    throw new RuntimeException("Server could not be started.");
+                }
+            } catch (InterruptedException ignored) {
+            }
+        }
     }
 }
